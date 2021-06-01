2c01f53f9fc1b2074070dfb4e9668c8854974ec7
Norrie F <nfurukawa1210@gmail.com>
2021-06-01 14:43:37 +0900

    Add files via upload

---
+++ chapter2.py
@@ -0,0 +1,8 @@
+import math
+import mldatasets
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
